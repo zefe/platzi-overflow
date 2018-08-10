a97a2fbc7e98d273011a9cdcc7b5249362a09dc8
zefetorres <zefetorres@icloud.com>
2018-08-10 00:17:04 -0500

    Add header

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const modules = [
-  MatButtonModule,
-  MatCheckboxModule
+  MatToolbarModule,
+  MatIconModule
 ];
 
 @NgModule({
