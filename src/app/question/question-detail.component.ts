1181735189545cbecb876585b3f11a20eb02211b
zefetorres <zefetorres@icloud.com>
2018-08-10 23:19:01 -0500

    Add question detail.

---
+++ src/app/question/question-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-deatil',
+  templateUrl: './question-deatil.component.html'
+})
+export class QuestionDetailComponent {
+
+}
