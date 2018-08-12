5da99511faac65472c4006203b9764355568324b
zefetorres <zefetorres@icloud.com>
2018-08-12 11:59:56 -0500

    hoe to show the list answers

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from './answer.model';
+import { Question} from '../question/question.model';
+
 @Component({
   selector: 'app-answer-form',
   templateUrl: './answer-form.component.html',
@@ -14,7 +17,15 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AnswerFormComponent {
+    @Input() question: Question;
     onSubmit(form: NgForm) {
-      console.log(form.value.description);
+      const answer = new Answer(
+        form.value.description,
+        this.question,
+        new Date(),
+        new User('Paula', 'Becerra')
+      );
+      this.question.answers.unshift(answer);
+      form.reset();
     }
 }
