a99bcc06bddc81afa9305588ecca781b1154bd71
zefetorres <zefetorres@icloud.com>
2018-08-11 23:27:30 -0500

    create answer model

--- src/app/question/question.model.ts
+++ src/app/question/question.model.ts
@@ -1,8 +1,11 @@
+import { Answer } from '../answer/answer.model';
+
 export class Question {
   title: string;
   description: string;
   createAt?: Date;
   icon?: string;
+  answers: Answer[];
 
   constructor(
     title: string,
@@ -14,5 +17,6 @@ export class Question {
     this.description = description;
     this.createAt = createAt;
     this.icon = icon;
+    this.answers = [];
   }
 }
